ec4e020c7f32f03a3ddec1348f09d028a15be03e
Jason Diamond <jason@diamond.name>
2016-07-17 21:18:47 -0700

    load TSV file and convert to JSON on the fly

--- app/script/i18n.js
+++ app/script/i18n.js
@@ -1,4 +1,6 @@
 $(function() {
+    var unflatten = nodeRequire('flat').unflatten;
+
     // This function loads all files required for a locale and sets it as the default..
     // Returns a Promise.
     // Examples of locale IDs can be found here:
@@ -24,10 +26,11 @@ $(function() {
     var init = _.once(function() {
         // First we load the supplemental CLDR data we need.
         return $.when.apply($, supplementalFiles()).then(function() {
-            console.log('all supplemantal CLDR data loaded');
+            console.log('all supplemental CLDR data loaded');
         }).then(function() {
             // Then we load the default messages.
-            return messageFile('root');
+            // return messageFile('root');
+            return tsvMessageFile();
         });
     });
 
@@ -37,7 +40,7 @@ $(function() {
         return $.when.apply($, mainFiles(locale)).then(function() {
             console.log('all main files loaded for ' + locale);
         }).then(function() {
-            return messageFile(locale);
+            // return messageFile(locale);
         });
     });
 
@@ -81,5 +84,45 @@ $(function() {
         }, function(err) {
             console.error('there is probably an error in your JSON, buddy');
         });
+    }
+
+    // Load a TSV file and convert it to the JSON format required by Globalize.js.
+    // The first column is the key.
+    // The second column is English and will be used as the "root" messages.
+    // All columns after that need to be labeled with their locale.
+    // de for German, es for Spanish, fr for French, etc.
+    // The file is supposed to be located in
+    function tsvMessageFile() {
+        var file = 'messages/messages.tsv';
+        return $.get(file).then(function(result) {
+            var json = convertTSVtoJSON(result);
+            console.log('messages:');
+            console.log(json);
+            Globalize.loadMessages(json);
+            console.log('loaded ' + file);
+        }, function(err) {
+            console.error('error loading ' + file);
+        });
+    }
+
+    function convertTSVtoJSON(tsv) {
+        var lines = _.map(tsv.split('\n'), line => line.trim()).filter(Boolean);
+        var header = lines.shift();
+        var locales = header.split('\t').slice(1);
+
+        // Rename English to "root" so other languages fall back to English:
+        locales[0] = 'root';
+
+        var translations = _.map(lines, line => line.split('\t'));
+
+        var messages = _.object(_.map(locales, (locale, i) => [
+            locale,
+            unflatten(_.object(_.map(translations, t => [ t[0], t[i + 1] ])), { delimiter: '/' })
+        ]));
+
+        // English translations are in the "root" locale so just add an empty object for English:
+        messages.en = {};
+
+        return messages;
     }
 });
