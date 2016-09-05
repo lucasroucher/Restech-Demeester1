8cec05d31b35e649b23113b8e3d74cc047a14e1d
Jason Diamond <jason@diamond.name>
2016-09-05 03:21:09 -0700

    copy messages to C:\Restech\DataView on install

--- app/install.js
+++ app/install.js
@@ -1,5 +1,7 @@
-const DATA_DIR = 'C:\\Restech\\DataView\\Data';
+const DATAVIEW_DIR = 'C:\\Restech\\DataView';
+const DATA_DIR = DATAVIEW_DIR + '\\Data';
 const SAMPLES_DIR = __dirname + '/data/*';
+const MESSAGES_FILE = __dirname + '/messages/messages.tsv';
 
 // This function gets run when the app is installed.
 module.exports = function(app, done) {
@@ -7,6 +9,7 @@ module.exports = function(app, done) {
 
     shelljs.mkdir('-p', DATA_DIR);
     shelljs.cp('-nRf', SAMPLES_DIR, DATA_DIR);
+    shelljs.cp(MESSAGES_FILE, DATAVIEW_DIR);
 
     done();
 };
